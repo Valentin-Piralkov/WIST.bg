fe15d1b03e86b7c6c62d4c8c7f131343a3fe1160
Valentin Piralkov <valiopiralkov@gmail.com>
2024-09-24 11:04:52 +0300

    login page

--- app/components/login/loginCard.tsx
+++ app/components/login/loginCard.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FaFacebook, FaGoogle, FaApple, FaEye, FaEyeSlash } from "react-icons/fa";
 
-export default function UserRegistrationCard() {
+export default function UserLoginCard() {
   const { t } = useTranslation();
 
   const [showPassword, setShowPassword] = useState(false);
@@ -39,32 +39,16 @@ export default function UserRegistrationCard() {
 
   return (
     <div className="flex flex-col px-8 py-6 w-1/3 justify-center items-center font-title font-medium text-xl">
-      <h2>{t("create_profile")}</h2>
+      <h2>{t("login_text")}</h2>
       <div className="flex flex-row w-full justify-center items-center gap-4 mt-4">
-        <h5>{t("create_profile_text")}</h5>{" "}
-        <a href="/login" className="text-orange hover:underline">
+        <h5>{t("dont_have_account")}</h5>{" "}
+        <a href="/register" className="text-orange hover:underline">
           {" "}
-          <h5> {t("login")} </h5>
+          <h5> {t("create_profile")} </h5>
         </a>
       </div>
 
-      <Form method="post" className="space-y-6 mt-8">
-        <div>
-          <label htmlFor="name" className="block text-sm font-medium">
-            {t("first_last_name")}
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder={t("first_last_name_placeholder")}
-            className="mt-1 block w-full px-3 py-2 font-normal border border-gray-light rounded-md shadow-sm sm:text-sm focus:outline-none focus:ring-2 focus:ring-orange focus:border-transparent"
-            required
-          />
-        </div>
-
+      <Form method="post" className="space-y-6 mt-8 w-full">
         <div>
           <label htmlFor="email" className="block text-sm font-medium">
             {t("email")}
@@ -82,24 +66,8 @@ export default function UserRegistrationCard() {
         </div>
 
         <div>
-          <label htmlFor="phone" className="block text-sm font-medium">
-            {t("phone")}
-          </label>
-          <input
-            type="tel"
-            name="phone"
-            id="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            placeholder={t("phone_placeholder")}
-            className="mt-1 block w-full px-3 py-2 font-normal border border-gray-light rounded-md shadow-sm sm:text-sm focus:outline-none focus:ring-2 focus:ring-orange focus:border-transparent"
-            required
-          />
-        </div>
-
-        <div>
           <label htmlFor="password" className="block text-sm font-medium">
-            {t("create_password")}
+            {t("password")}
           </label>
           <div className="relative w-full">
             <input
@@ -124,43 +92,15 @@ export default function UserRegistrationCard() {
               )}
             </button>
           </div>
-          <p className="text-xs">{t("password_instructions")}</p>
+          <p className="text-xs text-right hover:underline mt-2">{t("forgot_password")}</p>
         </div>
-
-        <div>
-          <label htmlFor="repeat_password" className="block text-sm font-medium">
-            {t("repeat_password")}
-          </label>
-          <input
-            type={showPassword ? "text" : "password"}
-            name="repeat_password"
-            id="repeat_password"
-            value={formData.repeat_password}
-            onChange={handleChange}
-            placeholder={t("repeat_password_placeholder")}
-            className="mt-1 block w-full px-3 py-2 font-normal border border-gray-light rounded-md shadow-sm sm:text-sm focus:outline-none focus:ring-2 focus:ring-orange focus:border-transparent"
-            required
-          />
-        </div>
-
-        <p className="text-sm mt-4">
-          {t("by_clicking")}{" "}
-          <a href="/" className="font-bold hover:underline">
-            {t("terms_and_conditions")}
-          </a>{" "}
-          {t("and")}{" "}
-          <a href="/" className="font-bold hover:underline">
-            {t("privacy_policy")}
-          </a>
-          .
-        </p>
 
         <button
           type="submit"
           className="w-full py-2 px-4 rounded-md mt-4 bg-blue-light hover:underline font-title text-white disabled:bg-gray-light disabled:cursor-not-allowed disabled:hover:no-underline"
           disabled={!isFormValid}
         >
-          {t("create_profile")}
+          {t("login")}
         </button>
       </Form>
 
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,1 +1,19 @@
+import { useActionData } from "@remix-run/react";
+import { contactFormAction } from "~/.server/actions/index/contactForm";
+import IndexHeader from "~/components/header";
+import IndexFooter from "~/components/footer";
+import UserLoginCard from "~/components/login/loginCard";
 
+export const action = contactFormAction;
+
+export default function RegisterUser() {
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <div className="flex flex-col justify-between items-center min-h-[100vh] h-full w-full bg-white gap-20">
+      <IndexHeader />
+      <UserLoginCard />
+      <IndexFooter actionData={actionData} />
+    </div>
+  );
+}
